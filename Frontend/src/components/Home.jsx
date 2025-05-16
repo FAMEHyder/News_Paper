9c63168a620a2f38bde6d735bbda2052469813c7
Fame hyder <famehyder9999@gmail.com>
2025-05-16 08:23:05 -0700

    UDB

--- Frontend/src/components/Home.jsx
+++ Frontend/src/components/Home.jsx
@@ -1,4 +1,4 @@
-import { Box, Container, } from '@mui/material';
+import { Box, Card, Container, CardMedia, CardContent } from '@mui/material';
 import { useState, useEffect } from 'react';
 import bg from '../Image/bg.png';
 import paper from '../Image/paper.png';
@@ -10,49 +10,38 @@ const Home = () => {
   // 
   return (
     <Container
-      maxWidth={false}
-      disableGutters
       sx={{
-  flex: 1,
-  position: 'relative',
-  height: { xs: '120vh', sm: '150vh', md: '200vh' }, // Responsive height
-  '&::before': {
-    content: '""',
-    position: 'absolute',
-    top: 0,
-    bottom: 0,
-    left: 0,
-    right: 0,
-    backgroundSize: 'cover',
-    backgroundPosition: 'center',
-    backgroundRepeat: 'no-repeat',
-    backgroundImage: `url(${bg})`,
-    zIndex: -2,
-  },
-  '&::after': {
-    content: '""',
-    position: 'absolute',
-    top: 0,
-    bottom: 0,
-    left: 0,
-    right: 0,
-    backgroundColor: 'rgba(0,0,0,.5)',
-    zIndex: -1,
-  }
-}}
-
-    >
+        height: '150vh'
+      }}>
       {/* the first main box of the container  */}
       <Box>
         {/* inner first box */}
-        <Box
-        component={'img'}
-        src={paper}
-        sx={{
+        <Card
+          sx={{
+            mt:20,
+            width:'55vh'
+          }}
 
-        }}
         >
-        </Box>
+          <CardContent>
+          کراچی
+          </CardContent>
+          <CardMedia
+            component={'img'}
+            src={paper}
+            sx={{
+              height:'45vh',
+            backgroundSize: 'contained',
+              ':hover': {
+                boxShadow: '2px 2px 2px 2px black',
+                transition: ' all 0.3s ease',
+                transform: 'scale(0.99)'
+              }
+            }}
+          />
+
+
+        </Card>
         {/* inner second box */}
         <Box>
         </Box>
@@ -61,7 +50,7 @@ const Home = () => {
         </Box>
       </Box>
 
-    </Container>
+    </Container >
   );
 };
 
