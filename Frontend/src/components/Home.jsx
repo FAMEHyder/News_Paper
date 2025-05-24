134146f25304689cef6a0d03f61ddff4ff3ba184
Fame hyder <famehyder9999@gmail.com>
2025-05-24 10:07:21 -0700

    UDB

--- Frontend/src/components/Home.jsx
+++ Frontend/src/components/Home.jsx
@@ -12,25 +12,73 @@ const Home = () => {
   return (
     <Container
       sx={{
-        height: '170vh',
+        height: '350vh',
 
       }}>
       {/* this is the main box  */}
       <Box>
         {/* the box from where the news paper is being read  */}
         <Box
-        
-        componont='img'
-        src={Main}
-        
-        >
 
+          componont='img'
+          src={Main}
+          sx={{
+            height: '300vh',
+            widht: '100%',
+            mt: '35vh',
+            border: '2px solid red'
 
-        </Box>
+          }}
+        />
 
         {/* the sub parts of the news paper  */}
-        <Box>
+        <Box
+          sx={{
+            height: '30vh',
+            widht: '100%',
+            mt:2,            
+            border: '2px solid red',
+            display:'flex',
+            
 
+          }}
+        >
+          <Box
+          sx={{
+            height: '3vh',
+            widht: '30vh',
+            mt:2,            
+            border: '2px solid red'
+
+          }}
+          />
+          <Box
+          sx={{
+            height: '3vh',
+            widht: '30vh',
+            mt:2,            
+            border: '2px solid red'
+
+          }}
+          />
+          <Box
+          sx={{
+            height: '3vh',
+            widht: '30vh',
+            mt:2,            
+            border: '2px solid red'
+
+          }}
+          />
+          <Box
+          sx={{
+            height: '3vh',
+            widht: '30vh',
+            mt:2,            
+            border: '2px solid red'
+
+          }}
+          />
 
         </Box>
 
--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -42,8 +42,8 @@ const Navbar = () => {
             }}
           >
             <Button color='white' sx={{mr:3,'&:hover':{bgcolor:'red',transition:'ease-in-out scale(.9)',}}}>صفحہ اول</Button>
-            <Button color='white' sx={{borderInlineStart:'2px solid white',mr:3,'&:hover':{bgcolor:'red',transition:'ease-in-out scale(.9)',}}}>صفحات</Button>
-            <Button color='white' sx={{borderInlineStart:'2px solid white',mr:3,'&:hover':{bgcolor:'red',transition:'ease-in-out scale(.9)',}}}>گزشتہ شمارے</Button>
+            <Button color='white' sx={{borderInlineStart:'2px solid silver',mr:3,'&:hover':{bgcolor:'red',transition:'ease-in-out scale(.9)',}}}>صفحات</Button>
+            <Button color='white' sx={{borderInlineStart:'2px solid silver',mr:3,'&:hover':{bgcolor:'red',transition:'ease-in-out scale(.9)',}}}>گزشتہ شمارے</Button>
           </Box>
 
         </Toolbar>
