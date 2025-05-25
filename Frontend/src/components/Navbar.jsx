2beac4f958e59992ae7f4daf0823adbe8dd427dd
Fame hyder <famehyder9999@gmail.com>
2025-05-25 10:33:26 -0700

    UDB

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -3,6 +3,12 @@ import { AppBar, Toolbar, Box, Button } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 import DMT from '../Image/DMT.png';
 import { Home } from '@mui/icons-material';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+import { YouTube } from '@mui/icons-material';
+
 const Navbar = () => {
   const navigate = useNavigate();
   const handleClick = (path) => {
@@ -29,23 +35,39 @@ const Navbar = () => {
               backgroundRepeat: 'no-repeat',
               width: '100%',
               backgroundPosition: 'cover',
-              borderBottom:'4px solid red',
+              borderBottom: '4px solid red',
             }}
           />
-          
+
           <Box
             sx={{
               height: '10vh',
               width: '100%',
               bgcolor: 'black'
-              
+
             }}
           >
-            <Button color='white' sx={{mr:3,'&:hover':{bgcolor:'red',transition:'ease-in-out scale(.9)',}}}>صفحہ اول</Button>
-            <Button color='white' sx={{borderInlineStart:'2px solid silver',mr:3,'&:hover':{bgcolor:'red',transition:'ease-in-out scale(.9)',}}}>صفحات</Button>
-            <Button color='white' sx={{borderInlineStart:'2px solid silver',mr:3,'&:hover':{bgcolor:'red',transition:'ease-in-out scale(.9)',}}}>گزشتہ شمارے</Button>
+            <Button color='white' sx={{ mr: 3, '&:hover': { bgcolor: 'red', transition: 'ease-in-out scale(.9)', } }}>صفحہ اول</Button>
+            <Button color='white' sx={{ borderInlineStart: '2px solid silver', mr: 3, '&:hover': { bgcolor: 'red', transition: 'ease-in-out scale(.9)', } }}>صفحات</Button>
+            <Button color='white' sx={{ borderInlineStart: '2px solid silver', mr: 3, '&:hover': { bgcolor: 'red', transition: 'ease-in-out scale(.9)', } }}>گزشتہ شمارے</Button>
           </Box>
-
+          <Box display={'flex'} flexDirection={'row'} justifyContent={'space-around'}>
+            <Link href="https://www.facebook.com" target="_blank" color="inherit">
+              <FacebookIcon sx={{ fontSize: 40, '&:hover': { color: 'blue' } }} />
+            </Link>
+            <Link href="https://www.twitter.com" target="_blank" color="inherit">
+              <TwitterIcon sx={{ fontSize: 40, '&:hover': { color: '#1da1f2' } }} />
+            </Link>
+            <Link href="https://www.instagram.com" target="_blank" color="inherit">
+              <InstagramIcon sx={{ fontSize: 40, '&:hover': { color: '#e1306c' } }} />
+            </Link>
+            <Link href="https://www.instagram.com" target="_blank" color="inherit">
+                <WhatsAppIcon sx={{ fontSize: 40, '&:hover': { color: 'green' } }} />
+              </Link>
+              <Link href="https://www.youtube.com" target="_blank" color="inherit">
+                <YouTube sx={{ fontSize: 40, '&:hover': { color: 'red' } }} />
+              </Link>
+          </Box>
         </Toolbar>
       </AppBar>
     </>
